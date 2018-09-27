bc4a92fd1cfa68658f2bfdc78b9be7995b731e5f
IFZR <infz3r0@gmail.com>
2018-09-27 21:41:17 +0700

    Testing delete

--- WPFTest/MainWindow.xaml.cs
+++ WPFTest/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Data.Entity;
+
 namespace WPFTest
 {
     /// <summary>
@@ -20,9 +22,41 @@ namespace WPFTest
     /// </summary>
     public partial class MainWindow : Window
     {
+        private WPFTestEntities1 db = new WPFTestEntities1();
+
+        CollectionViewSource cityViewSource;
+        CollectionViewSource personViewSource;
+
         public MainWindow()
         {
             InitializeComponent();
+            cityViewSource = ((CollectionViewSource)(this.FindResource("cityViewSource")));
+            personViewSource = ((CollectionViewSource)(this.FindResource("personViewSource")));
+            DataContext = this;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            cityViewSource.Source = db.cities.ToList();
+
+            db.people.Load();
+            personViewSource.Source = db.people.Local;
+        }
+
+        private void btnDeleteCity_Click(object sender, RoutedEventArgs e)
+        {
+            city o = (city)cityDataGrid.SelectedCells[0].Item;
+            try
+            {
+                db.cities.Remove(o);
+                db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                Console.WriteLine(ex.ToString());
+            }
+            cityViewSource.View.Refresh();
         }
     }
 }
